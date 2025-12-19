c3c2709edfe0baa9e38e4eebe1d547f62dd26bc1
KishoreKumar481 <kishore7302520@gmail.com>
2025-12-19 06:06:30 +0530

    7 2 8 added

---
+++ 7. Arrays/2. Medium/8-prod-of-arr-expext-self/1-optimal.py
@@ -0,0 +1,15 @@
+def productExceptSelf(nums):
+    n = len(nums)
+    res = [1] * n
+    prefix = 1
+    for i in range(n):
+        res[i] = prefix
+        prefix = prefix * nums[i]
+    postfix = 1
+    for i in range(n - 1, -1, -1):
+        res[i] = res[i] * postfix
+        postfix = postfix * nums[i]
+    return res
+
+nums = [1, 2, 3, 4]
+print(productExceptSelf(nums))  # [24, 12, 8, 6]
